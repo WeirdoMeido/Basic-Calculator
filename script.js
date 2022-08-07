5828fd17037f9311401266d7470df101da43b6c4
WeirdoMeido <meiren8adiaz@gmail.com>
2022-08-07 11:38:20 -0500

    Changed number divs to buttons and linked the script

---
+++ script.js
@@ -0,0 +1,25 @@
+const currentOperation = document.querySelector(".current-operation");
+const lastOperation = document.querySelector(".pre-operation");
+const numberBtn = document.querySelectorAll(".number-key");
+
+
+
+
+
+////FUNCTIONS////
+
+function add (a, b) {
+    return a + b;
+}
+
+function substract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
